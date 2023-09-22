29dca33847e37b6d6462ad4a044e32e3385a9042
manishgowda1698 <manishgowda1698@gmail.com>
2023-09-22 20:37:09 +0530

    Updated canges

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Logo from '../assets/logos.png';
+
+const Header = () => {
+  return(
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a href='#'>
+            <img src={Logo} alt='' />
+          </a>
+          <button className='btn btn-sm'>work with me</button>
+        </div>
+      </div>
+    </header>
+  ) 
+};
+
+export default Header;
